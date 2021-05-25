7c49df2d493e55c9438133d3b942a1ab572796ce
José María <46092575+josemaria28@users.noreply.github.com>
2021-05-25 22:15:57 +0200

    XX

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BannerRegisterComponent } from './banner-register/banner-register.component';
+import { InfoDesUnoComponent } from './info-des-uno/info-des-uno.component';
+import { InfoCardsCComponent } from './info-cards-c/info-cards-c.component';
+import { InfoCardsRComponent } from './info-cards-r/info-cards-r.component';
 
 
 
 @NgModule({
-  declarations: [PageNotFoundComponent],
+  declarations: [PageNotFoundComponent, BannerRegisterComponent, InfoDesUnoComponent, InfoCardsCComponent, InfoCardsRComponent],
   imports: [
     CommonModule
   ]
