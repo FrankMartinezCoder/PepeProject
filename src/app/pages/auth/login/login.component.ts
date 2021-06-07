c772cc4cf498f4458aac3e9f47889c9dd9b38e28
FrankMartinezCoder <martinecz1997@hotmail.com>
2021-06-07 22:06:05 +0200

    login validation

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import * as $ from 'jquery';
 import { UserService } from 'src/app/services/user-service.service';
@@ -14,16 +14,18 @@ export class LoginComponent {
   public password:string = '';
 
   public emailError:string = '';
-  public passwordError:string = '';
-  
-  constructor(private userService:UserService, private router:RouterModule) { }
+  public passwordError:string = ''; 
 
+  public buttonValidator:boolean = true;
+  constructor(private userService:UserService, private router:RouterModule) { }
 
   private reset():void {
     this.email = '';
     this.password = '';
     $("#login-component,#login-background").attr("hidden","true");
     $(".button-ok").removeAttr('loading');
+    this.emailError = "";
+    this.passwordError = "";
   }
 
   public loginCheck():void {
@@ -45,5 +47,45 @@ export class LoginComponent {
 
   public cancel():void {
     this.reset();
+  }
+
+  public emailValidator():void {
+    let temp = false;
+    if(!this.email) {
+      this.emailError = "El email es un campo obligatorio.";
+    }
+    else {
+      try {
+      if(!new RegExp(`^[a-z0-9._%+-]+@[a-z0-9.-]{4,20}\.[a-z]{2,4}$`).test(this.email)) {
+          throw new Error();
+        }
+        this.emailError = "";
+        temp = true;
+      }
+      catch(err) {           
+        this.emailError = "El campo email no es válido.";
+        temp = false;
+      }
+    }
+    this.buttonValidator = temp;
+  }
+  
+  public passwordValidator():void {
+    let temp = true;
+    if(!this.password) {
+      this.passwordError = "La contraseña es un campo obligatorio";
+      temp = false;
+    }
+    else {
+      this.passwordError = "";
+    }
+    this.buttonValidator = temp;
+  }
+
+  private validateButton(isValid:boolean):void {
+    if(isValid) {
+      this.passwordError = "";
+      this.emailError = "";
+    }
   }
 }
