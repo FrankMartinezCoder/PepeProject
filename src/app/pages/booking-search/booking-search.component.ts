aabba786b01196a7e856bd9b980d66bf32f978ae
FrankMartinezCoder <martinecz1997@hotmail.com>
2021-06-08 17:31:50 +0200

    xz

--- src/app/pages/booking-search/booking-search.component.ts
+++ src/app/pages/booking-search/booking-search.component.ts
@@ -8,11 +8,66 @@ import { BookingFilter } from 'src/app/model/front-model/BookingFilter';
 })
 export class BookingSearchComponent implements OnInit {
 
-  public filter:BookingFilter = new BookingFilter();
+  public filter: BookingFilter = new BookingFilter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public plus(elem: number) {
+    let data:number = elem==1?this.filter.numOccupant:(elem==2?this.filter.minPrice:this.filter.maxPrice);
+    
+    if (data < 99) {
+      data++;
+    }
+    if (data < 1) {
+      data = 1;
+    }
+    switch (elem) {
+      case 1:
+        this.filter.numOccupant = data;
+        break
+      case 2:
+        this.filter.minPrice = data;
+        break
+      case 3:
+        this.filter.maxPrice = data;
+        break
+    }
+  }
+
+  public less(elem: number) {
+    let data:number = elem==1?this.filter.numOccupant:(elem==2?this.filter.minPrice:this.filter.maxPrice);
+  
+    if (data > 1) {
+      data--;
+    }
+    if (data > 99) {
+      data = 99;
+    }
+
+    switch (elem) {
+      case 1:
+        this.filter.numOccupant = data;
+        break
+      case 2:
+        this.filter.minPrice = data;
+        break
+      case 3:
+        this.filter.maxPrice = data;
+        break
+    }
+  }
+  public checkNumOccupant() {
+    if (this.filter.numOccupant > 99) {
+      this.filter.numOccupant = 99;
+    }
+    if (this.filter.numOccupant < 1) {
+      this.filter.numOccupant = 1;
+    }
+  }
+  public clearData() {   
+    this.filter.clear();
+  }
 }
